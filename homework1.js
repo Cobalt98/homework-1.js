727c17092f17291eb4cc35905e673cf02a04ef20
Cobalt98 <alexschultz258@gmail.com>
2025-02-26 18:44:55 -0800

    Add files via upload

---
+++ homework1.js
@@ -0,0 +1,15 @@
+//All numbers divisable by 5 
+
+
+let n1 = 25
+let n2 = 15
+let n3 = 5
+let n4 = 3
+
+const isDivisableBy5 = (n1%5===0&&n2%5===0&&n3%5===0&&n4%5===0)
+
+console.log(isDivisableBy5)
+
+//the result has one left over 
+
+console("All variables divisbale by 5" + const name here)
